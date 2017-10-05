57856a40022eb455bc7ab2f9b543f8977c16bfb5
Vinzent Höfler <ada.rocks@jlfencey.com>
2017-10-05 10:31:37 +0200

    + License information
    + Added copyright and license information to the sources.

--- src/phelix/crypto-phelix.ads
+++ src/phelix/crypto-phelix.ads
@@ -1,3 +1,22 @@
+------------------------------------------------------------------------------
+--  Source code copyright (C) 2017 by Heisenbug Ltd.                        --
+--                                                                          --
+--  DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE                             --
+--                      Version 2, December 2004                            --
+--                                                                          --
+--   Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>                       --
+--                                                                          --
+--   Everyone is permitted to copy and distribute verbatim or modified      --
+--   copies of this license document, and changing it is allowed as long    --
+--   as the name is changed.                                                --
+--                                                                          --
+--              DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE                 --
+--     TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION      --
+--                                                                          --
+--    0. You just DO WHAT THE FUCK YOU WANT TO.                             --
+------------------------------------------------------------------------------
+
+------------------------------------------------------------------------------
 --  SPARK implementation of PHELIX.
 --
 --  A.  Cover sheet for Phelix Submission to ECRYPT
@@ -33,6 +52,7 @@
 --      Postal Address:                 1090A La Avenida
 --                                      Mountain View, CA 94043    USA
 --      E-mail Address:                 schneier@counterpane.com
+------------------------------------------------------------------------------
 
 with Interfaces;
 
