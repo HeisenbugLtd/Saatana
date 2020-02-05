6f6d925dba1774b3d35317d5d143f64aefc25f64
Vinzent Höfler <ada.rocks@jlfencey.com>
2020-02-05 10:27:32 +0100

    Create ci-github.sh

---
+++ scripts/ci-github.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+trap 'echo "ERROR at line ${LINENO} (code: $?)" >&2' ERR
+trap 'echo "Interrupted" >&2 ; exit 1' INT
+
+set -o errexit
+set -o nounset
+
+export PATH+=:${PWD}/bin
+
+# Build test_phelix
+gprbuild -j0 -p -P security.gpr
+
+# For the record
+echo ENVIRONMENT:
+env | sort
+echo ............................
+
+echo GNAT VERSION:
+gnatls -v
+echo ............................
+
+#echo ALR VERSION:
+#alr version
+#echo ............................
+
+echo TESTSUITE:
+cd _build
+./test_phelix
