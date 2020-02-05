3876316efbbb11a49e20388de65399a5bd797656
Vinzent Höfler <ada.rocks@jlfencey.com>
2020-02-05 13:33:11 +0100

    Update ci-github.sh

--- scripts/ci-github.sh
+++ scripts/ci-github.sh
@@ -27,7 +27,9 @@ echo TESTSUITE:
 
 echo "Checking provers..."
 
-(which altergo && echo `altergo --version`) || true
-(which cvc4 && echo `cvc4 --version`) || true
-(which z3 && echo `z3 --version`) || true
+SPARKDIR=/opt/gnat/libexec/spark/bin
+
+(test -x ${SPARKDIR}/alt-ergo && echo `${SPARKDIR}/alt-ergo -version`) || true
+(test -x ${SPARKDIR}/cvc4 && echo `${SPARKDIR}/cvc4 --version`) || true
+(test -x ${SPARKDIR}/z3 && echo `${SPARKDIR}/z3 -version`) || true
 #(which gnatprove && gnatprove -P security.gpr) || true
