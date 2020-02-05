dd0d31748b11ff95c780777909b42479e2f443ac
Vinzent Höfler <ada.rocks@jlfencey.com>
2020-02-05 16:45:34 +0100

    Create ci-test.sh

---
+++ scripts/ci-test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+trap 'echo "ERROR at line ${LINENO} (code: $?)" >&2' ERR
+trap 'echo "Interrupted" >&2 ; exit 1' INT
+
+set -o errexit
+set -o nounset
+
+echo "Running KAT:"
+./_build/test_phelix
