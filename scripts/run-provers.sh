0b4f28ad36fc158bba789736a8d7caa34e7bfd59
Vinzent Höfler <ada.rocks@jlfencey.com>
2020-02-05 16:40:34 +0100

    Create run-provers.sh

---
+++ scripts/run-provers.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+trap 'echo "ERROR at line ${LINENO} (code: $?)" >&2' ERR
+trap 'echo "Interrupted" >&2 ; exit 1' INT
+
+set -o errexit
+set -o nounset
+
+echo "Checking provers..."
+
+SPARKDIR=/opt/gnat/libexec/spark/bin
+
+(test -x ${SPARKDIR}/alt-ergo && echo `${SPARKDIR}/alt-ergo -version`) || true
+(test -x ${SPARKDIR}/cvc4 && echo `${SPARKDIR}/cvc4 --version`) || true
+(test -x ${SPARKDIR}/z3 && echo `${SPARKDIR}/z3 -version`) || true
+
+(which gnatprove && gnatprove -P security.gpr) || true
