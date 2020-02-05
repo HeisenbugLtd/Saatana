dc6dd17f399d9acc7a90e356257a57c22c4451e3
Vinzent Höfler <ada.rocks@jlfencey.com>
2020-02-05 16:41:07 +0100

    Update and rename ci-github.sh to ci-build.sh

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -24,13 +24,3 @@ echo ............................
 
 echo TESTSUITE:
 ./_build/test_phelix
-
-echo "Checking provers..."
-
-SPARKDIR=/opt/gnat/libexec/spark/bin
-
-(test -x ${SPARKDIR}/alt-ergo && echo `${SPARKDIR}/alt-ergo -version`) || true
-(test -x ${SPARKDIR}/cvc4 && echo `${SPARKDIR}/cvc4 --version`) || true
-(test -x ${SPARKDIR}/z3 && echo `${SPARKDIR}/z3 -version`) || true
-
-(which gnatprove && gnatprove -P security.gpr) || true
