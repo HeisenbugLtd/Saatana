81f3789012c4c5e65499fac8a879d0cd25ad9d8d
Vinzent Höfler <ada.rocks@jlfencey.com>
2020-02-05 13:39:57 +0100

    + Output information about installed provers
    * Also, re-enabled the proof run.

--- scripts/ci-github.sh
+++ scripts/ci-github.sh
@@ -32,4 +32,5 @@ SPARKDIR=/opt/gnat/libexec/spark/bin
 (test -x ${SPARKDIR}/alt-ergo && echo `${SPARKDIR}/alt-ergo -version`) || true
 (test -x ${SPARKDIR}/cvc4 && echo `${SPARKDIR}/cvc4 --version`) || true
 (test -x ${SPARKDIR}/z3 && echo `${SPARKDIR}/z3 -version`) || true
-#(which gnatprove && gnatprove -P security.gpr) || true
+
+(which gnatprove && gnatprove -P security.gpr) || true
