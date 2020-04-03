78b43c370f306f338420bdc7ca655776df87b0cf
Vinzent "Jellix" Saranen <ada.rocks@jlfencey.com>
2020-04-03 09:15:31 +0200

    * Revert "* GNAT installation moved"
    This reverts commit d4349ffcf50bd470b1e757e8818c831d7a8d7b96.

--- .github/scripts/ci-build.sh
+++ .github/scripts/ci-build.sh
@@ -6,16 +6,14 @@ trap 'echo "Interrupted" >&2 ; exit 1' INT
 set -o errexit
 set -o nounset
 
-export PATH+=:${PWD}/bin
-
 # For the record
 echo ENVIRONMENT:
 env | sort
 echo ............................
 
-echo GNAT VERSION:
-gnatls -v
-echo ............................
+#echo GNAT VERSION:
+#gnatls -v
+#echo ............................
 
 # Build test_phelix
 gprbuild -j0 -p -P saatana.gpr
